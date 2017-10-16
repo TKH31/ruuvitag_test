3de8e2b631dd594879d0ed1910172701b25b2c3f
tkh-31 <tk.dev@outlook.com>
2017-10-16 14:35:43 +0900

    Update record_broadcast.py

--- record_broadcast.py
+++ record_broadcast.py
@@ -1,13 +1,17 @@
 from ruuvitag_sensor.ruuvitag import RuuviTag
 import time
-import sys
+import argparse
+
+parser = argparse.ArgumentParser(description='Check for infinite loop.')
+parser.add_argument("is_inifinte", action='store_const', nargs='?', default="", required=False)
+input = parser.parse_args()
 
 mac_addr = 'DE:E5:EA:C5:9C:3E'
 count = 0
 sensor = RuuviTag(mac_addr)
 
 while count < 2:
-    if sys.argv[1]:
+    if input.is_inifinte == "":
         count = count + 1
     state = sensor.update()
     myoutput = sensor.state
