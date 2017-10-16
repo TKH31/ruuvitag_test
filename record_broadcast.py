4d51a22e8e71ed1d82124e7db17873fb8f6bcd46
tkh-31 <tk.dev@outlook.com>
2017-10-16 14:18:35 +0900

    Update record_broadcast.py

--- record_broadcast.py
+++ record_broadcast.py
@@ -1,12 +1,16 @@
 from ruuvitag_sensor.ruuvitag import RuuviTag
 import time
+import sys
+
+arg_1 = sys.argv[1]
 
 mac_addr = 'DE:E5:EA:C5:9C:3E'
 count = 0
 sensor = RuuviTag(mac_addr)
 
 while count < 2:
-    count = count + 1
+    if arg_1 == "":
+        count = count + 1
     state = sensor.update()
     myoutput = sensor.state
     
