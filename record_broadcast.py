25e8dd0294cb6eb6d92f289d82d92d546ec95a04
tkh-31 <tk.dev@outlook.com>
2017-10-16 13:43:31 +0900

    Update record_broadcast.py

--- record_broadcast.py
+++ record_broadcast.py
@@ -1,35 +1,20 @@
 from ruuvitag_sensor.ruuvi import RuuviTagSensor
-
-
-
-
-
-
+mac_addr = 'DE:E5:EA:C5:9C:3E'
 count = 0
-def handle_data(found_data):
-    global count
+sensor = RuuviTag(mac_addr)
+
+while count < 2:
     count = count + 1
-
-
-
-    sensor = RuuviTag('DE:E5:EA:C5:9C:3E')
-
-
+    state = sensor.update()
+    myoutput = sensor.state
     
-
-
-
-state = sensor.update()
-state = sensor.state
-print(state)
-    
-    
-    
-    myoutput = found_data[1]
-    myoutput["MAC"] = str(found_data[0])
+    myoutput["MAC"] = mac_addr
     myoutput["count"] = count
+
     f = open("broadcast.log", "a")
     f.write(str(myoutput))
     f.close()
+    
     print(str(myoutput))
-RuuviTagSensor.get_datas(handle_data)
+
+print "End"
